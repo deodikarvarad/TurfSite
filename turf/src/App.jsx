11e22ff9ebc76e3911680a4a776b89bf07ccd46b
Varad Deodikar <deodikarvarad@gmail.com>
2024-08-11 22:35:16 +0530

    MSG

--- turf/src/App.jsx
+++ turf/src/App.jsx
@@ -8,23 +8,7 @@ function App() {
 
   return (
     <>
-      <form>
-  <img src="..." />
-    <div>
-    <h3>Basic Tee</h3>
-    <h3>$35</h3>
-    <fieldset>
-      <legend class="sr-only">Choose a color</legend>
-      <div class="forced-color-adjust-none ...">
-        <label >
-          <input class="sr-only" type="radio" name="color-choice" value="White" />
-          <span class="sr-only">White</span>
-          <span class="size-6 rounded-full border border-black border-opacity-10 bg-white"></span>
-        </label>
-      </div>
-    </fieldset>
-    </div>
-</form>
+      
     </>
   )
 }
