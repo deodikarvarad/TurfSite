9c7cdada50a97ef72a916b8aef9a6649091572b9
iamatharvak <kulkarni.atharva.1403@gmail.com>
2024-09-19 08:36:53 +0530

    commit-changes

--- turf/src/components/UserLogin/booking.jsx
+++ turf/src/components/UserLogin/booking.jsx
@@ -1,4 +1,8 @@
+
 import React from "react";
+import PlaceIcon from '@mui/icons-material/Place';
+import ScheduleIcon from '@mui/icons-material/Schedule';
+import PaymentsIcon from '@mui/icons-material/Payments';
 
 
 function Turfbooking(){
@@ -26,8 +30,8 @@ function Turfbooking(){
             />
             </div>
             </div>
-            <div className="relative flex flex-col  mt-8  border border-red-500 w-[1071px] h-[238px] bg-[#9ad5d3]  ">
-                <div className="flex justify-between w-full ">
+            <div className="relative flex flex-col  mt-8  w-[1071px] h-[238px] bg-[#9ad5d3]  ">
+                <div className="flex justify-between w-full  ">
                 <div className="flex justify-start  mt-5 ml-7 ">
                 <h2 className="text-black font-serif non-italic text-3xl font-bold ">Penalty Box</h2>
                 </div>
@@ -41,9 +45,9 @@ function Turfbooking(){
 
                 </div>
                 </div>
-                <div className=" flex mt-1  w-[1071px] h-[20px] align-top justify ">
-                <div className="flex justify-start ml-8">
-                <h1 className=" text-black font-serif text-xs font-bold ">5.0 ⭐⭐⭐⭐✨ (196)</h1>
+                <div className=" flex mt-3  w-[1071px]  align-top justify  border-b border-b-gray-600 border-b-dashed pb-4">
+                <div className="flex justify-start ml-8 ">
+                <h1 className=" text-black font-serif text-xs font-bold  ">5.0 ⭐⭐⭐⭐✨ (196)</h1>
                 </div>
                 <div className="flex ml-8">
                     <h1 className=" text-black font-serif text-xs font-bold">🔒 <span className=" text-yellow-600 ">Closed </span>Opens soon at 9:00am</h1>
@@ -55,9 +59,44 @@ function Turfbooking(){
                     <h1 className="text-black font-serif text-xs font-bold">📈 15 people booked recently</h1>
                 </div>
                  </div>
+                 <div className="flex mt-3  w-[1071px]  align-top justify ">
+                    <div className="flex ml-4 w-[350px] h-[72px]  ">
+                    <PlaceIcon className="w-[15px] h-[19.5px] mt-2 mr-2" />
+                    <div>
+                        <h2 className="text-black font-serif non-italic text-[16px] font-light ">
+                        Shivaji Nagar Sutgirni Road, Aurangabad 431001 India
+
+                        </h2>
+                    </div>
+                    </div>
+                    <div className="flex ml-3 w-[296px] h-[50px]  justify ">
+                    <div className="flex justify-start ml-2 ">
+                    <ScheduleIcon className="w-[20px] h-[20px]  mr-2"/>
+                    </div>
+                    <div>
+                    <h2 className="text-black font-serif non-italic text-[16px] font-light">Monday</h2>
+                    <h2 className="text-black font-serif non-italic text-[16px] font-light">Tue-Sun</h2>
+                    
+                    </div>
+                    <div className="ml-8">
+                    <h2 className="text-black font-serif non-italic text-[16px] font-light">Closed</h2>
+                    <h2 className="text-black font-serif non-italic text-[16px] font-light">7:00am-1:00am</h2>
+                    </div>
+                    </div>
+                    <div className="flex w-[257px] h-[70px]  ml-4">
+                    <div className="ml-2">
+                        <PaymentsIcon/>
+                    </div>
+                    <div className="ml-4">
+                        <h2 className="text-black font-serif non-italic text-[16px] font-light">Mode of Payment</h2>
+                        <h2 className="text-black font-serif non-italic text-[16px] font-light">Cash, Debit card, Credit card, UPI, Crypto</h2>
+                        
+                    </div>
+                    </div>
+                 </div>
             </div>
         </div>
-        </>
+        </> 
     )
 
     
