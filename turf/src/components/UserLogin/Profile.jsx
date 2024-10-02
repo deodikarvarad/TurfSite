976ba790073657b025c09b54d5e368d4faf100a4
Varad Deodikar <deodikarvarad@gmail.com>
2024-10-02 09:20:18 +0530

    profile

--- turf/src/components/UserLogin/Profile.jsx
+++ turf/src/components/UserLogin/Profile.jsx
@@ -4,8 +4,8 @@ import Virat from "../../assets/Virat.png";
 function ProfilePage(){
     return(
     <>
-    <div className="bg-yellow-100">
-        <div className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via bg-green-500 to-yellow-500 pt-40 pl-36 underline">
+    <div className="bg-white">
+        <div className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via bg-voilet-500 to-indigo-500 pt-40 pl-36 underline">
             User Profile
         </div>
         <div className="flex flex-row pt-6 pb-6 ml-40 mr-40 justify-between items-center ">
@@ -26,20 +26,20 @@ function ProfilePage(){
             <div>
                 <h2 className="ml-40 text-xl">First Name</h2>
                 <input type="First Name" 
-                className="ml-40 p-2 pr-56 rounded-lg"/>
+                className="ml-40 p-2 pr-56 rounded-lg bg-blue-100"/>
             </div>
 
             <div>
                 <h2 className="ml-40 text-xl">Last Name</h2>
                 <input type="Last Name"
-                className="ml-40 p-2 pr-56 rounded-lg" />
+                className="ml-40 p-2 pr-56 rounded-lg  bg-blue-100" />
             </div>     
             </div>
 
             <div className="ml-40 mr-40 pt-10">
             <h2 className="ml-40 text-xl">User Name</h2>
                 <input type="User Name" 
-                className="ml-40 p-2 pr-56 rounded-lg"/>
+                className="ml-40 p-2 pr-56 rounded-lg  bg-blue-100"/>
             </div>  
 
             <div className="w-full flex items-center justify-center relative pt-8">
@@ -50,13 +50,13 @@ function ProfilePage(){
             <div>
                 <h2 className="ml-40 text-xl">Email</h2>
                 <input type="email" name="Email" id="" 
-                className="ml-40 p-2 pr-56 rounded-lg"/>
+                className="ml-40 p-2 pr-56 rounded-lg  bg-blue-100"/>
             </div>
 
             <div>
                 <h2 className="ml-40 text-xl">Phone No.</h2>
                 <input type="Phone No."
-                className="ml-40 p-2 pr-56 rounded-lg" />
+                className="ml-40 p-2 pr-56 rounded-lg  bg-blue-100" />
             </div>     
             </div>
 
@@ -67,7 +67,7 @@ function ProfilePage(){
             <div className="ml-40 mr-40 pt-10 pb-2 ">
                 <h2 className="ml-40 text-xl">Loaction</h2>
                 <input type="Location"
-                className="ml-40 p-2 pr-96 rounded-lg" />
+                className="ml-40 p-2 pr-96 rounded-lg  bg-blue-100" />
             </div>  
 
             <div className="w-full flex items-center justify-center relative pt-8">
@@ -79,20 +79,20 @@ function ProfilePage(){
             <div>
                 <h2 className="ml-40 text-xl">Current Password</h2>
                 <input type="password" name="Current Password" id=""
-                className="ml-40 p-2 pr-56 rounded-lg" />
+                className="ml-40 p-2 pr-56 rounded-lg  bg-blue-100" />
             </div>
 
             <div>
                 <h2 className="ml-40 text-xl">New Password</h2>
                 <input type="password" name="New Password" id=""
-                className="ml-40 p-2 pr-56 rounded-lg" />
+                className="ml-40 p-2 pr-56 rounded-lg  bg-blue-100" />
             </div>     
             </div>
 
             <div className="ml-40 mr-40 pt-10 pb-10">
             <h2 className="ml-40 text-xl">Confirm New Password</h2>
             <input type="password" name="Confirm New Password" id=""
-                className="ml-40 p-2 pr-56 rounded-lg" />
+                className="ml-40 p-2 pr-56 rounded-lg  bg-blue-100" />
             </div>  
 
             <div className="flex flex-row justify-end items-end pb-10">
@@ -100,15 +100,6 @@ function ProfilePage(){
             <button className="mr-5 transition ease-in-out delay-150 text-white bg-gradient-to-r from-red-500 via-red-900 to-red-700 hover:-translate-y-1 hover:scale-110 hover:bg-red-500 duration-300">Cancel</button>
             <button className="mr-40 transition ease-in-out delay-150 text-white bg-gradient-to-r from-blue-500 via-violet-500 to-indigo-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300">Save Changes</button>
             </div>
-
-
-
-
-
-
-
-
-
     </div>
 
     </>
